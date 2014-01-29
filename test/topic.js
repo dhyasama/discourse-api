e73aa66b0d900292d9a22980ae910efd25ccbdcc
Jason Reynolds <dhyasama@gmail.com>
2014-01-29 11:18:14 -0500

    Add ability to update topic and post

--- test/topic.js
+++ test/topic.js
@@ -10,8 +10,10 @@ describe('Discourse Topic API', function() {
 
   var
     Discourse = require('../lib/discourse'),
-    api       = new Discourse(config.url, config.api.key, config.api.username),
-    topic_id  = '';
+    api = new Discourse(config.url, config.api.key, config.api.username),
+    topic_id = '',
+    slug = '',
+    post_id = '';
 
   it('creates a topic', function(done) {
 
@@ -20,7 +22,7 @@ describe('Discourse Topic API', function() {
 
     require('crypto').randomBytes(5, function(err, buf) {
 
-      api.createTopic(config.topic.title + ' ' + buf, config.topic.body, config.topic.category, function(err, body, httpCode) {
+      api.createTopic(config.topic.title + ' ' + buf.toString('hex').toUpperCase(), config.topic.body, config.topic.category, function(err, body, httpCode) {
 
         // make assertions
         should.not.exist(err);
@@ -36,6 +38,36 @@ describe('Discourse Topic API', function() {
 
         // save for subsequent tests
         topic_id = json.topic_id;
+        slug = json.topic_slug;
+
+        done();
+
+      });
+
+    });
+
+  });
+
+  it('updates a topic', function(done) {
+
+    // topic_id set in previous test
+
+    // append random string to title because discourse has a setting to prevent duplicate titles
+
+    require('crypto').randomBytes(5, function(err, buf) {
+
+      api.updateTopic(slug, topic_id, config.topic.title + ' UPDATE ' + buf.toString('hex').toUpperCase(), 'uncategorized', function(err, body, httpCode) {
+
+        // make assertions
+        should.not.exist(err);
+        should.exist(body);
+        httpCode.should.equal(200);
+
+        var json = JSON.parse(body);
+
+        // make more assertions
+        json.should.have.properties('basic_topic');
+        json.basic_topic.id.should.be.above(0);
 
         done();
 
@@ -62,9 +94,34 @@ describe('Discourse Topic API', function() {
       json.should.have.properties('id');
       json.id.should.be.above(0);
 
+      post_id = json.id;
+
       done();
 
     });
+  });
+
+  it('updates a post', function(done) {
+
+    // topic_id set in previous test
+
+    api.updatePost(post_id, 'i updated my post! horray!', 'testing', function(err, body, httpCode) {
+
+      // make assertions
+      should.not.exist(err);
+      should.exist(body);
+      httpCode.should.equal(200);
+
+      var json = JSON.parse(body);
+
+      // make more assertions
+      json.should.have.properties('post');
+      //json.basic_topic.id.should.be.above(0);
+
+      done();
+
+    });
+
   });
 
   it('replies to a post', function(done) {
@@ -111,23 +168,23 @@ describe('Discourse Topic API', function() {
     });
   });
 
-  it('deletes a topic', function(done) {
-
-    // topic_id set in previous test
-
-    api.deleteTopic(topic_id, function(err, body, httpCode) {
-
-      // make assertions
-      should.not.exist(err);
-      should.exist(body);
-      httpCode.should.equal(200);
-
-      // todo - check body
-
-      done();
-
-    });
-
-  });
+//  it('deletes a topic', function(done) {
+//
+//    // topic_id set in previous test
+//
+//    api.deleteTopic(topic_id, function(err, body, httpCode) {
+//
+//      // make assertions
+//      should.not.exist(err);
+//      should.exist(body);
+//      httpCode.should.equal(200);
+//
+//      // todo - check body
+//
+//      done();
+//
+//    });
+//
+//  });
 
 });
