877df838d3dd2f0e40574d0a60da77ca75135166
Yuan Hsiang Cheng <lyforever62@hotmail.com>
2015-01-12 14:39:20 +0800

    * Add category API * Add API of sync and category test

---
+++ test/sync.js
@@ -0,0 +1,26 @@
+"use strict";
+
+var
+  should  = require("should"),
+  fs      = require('fs'),
+  path    = require('path'),
+  config  = JSON.parse(fs.readFileSync(path.normalize(__dirname + '/config.json', 'utf8')));
+
+
+describe('Discourse Sync API', function() {
+
+  var
+    Discourse = require('../lib/discourse'),
+    api = new Discourse(config.url, config.api.key, config.api.username),
+
+  it('gets site.json synchronously', function(done) {
+
+    var res = api.getSync('/site.json');
+    res.statusCode.should.equal(200);
+
+    done();
+
+  });
+
+
+});
